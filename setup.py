32023f9e66d46a4cecdc481d8d2fe35a76b08ee7
meklenbpo <yk@kantynent.com>
2021-03-14 22:34:07 +0300

    Added basic installer

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+Setuptools installation for HereGeocoder.
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='HereGeocoder',
+    version='0.0.1',
+    description='Get point address from X/Y and vice versa',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
